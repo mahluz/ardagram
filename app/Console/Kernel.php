60e78ded6dcf0c18832eda7c3b9bb48b492738e4
zulham <zulham724@gmail.com>
2017-12-22 19:52:23 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('instagram:post')
-                 ->everyMinute();
+        // $schedule->command('instagram:post')
+        //          ->everyMinute();
     }
 
     /**
